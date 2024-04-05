e7704d60cb4c38fb33b6193b0645c2b499b511d8
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2024-04-05 14:54:31 +0200

    first commit

---
+++ src/main/java/com/cca/moodmeter/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.cca.moodmeter.config;
+
+import com.cca.moodmeter.config.security.JsonWebTokenAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+
+public class WebSecurityConfig {
+
+    @Autowired
+    JsonWebTokenAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        String[] unsecuredResources = new String[] { "/swagger-ui/**", "/v3/api-docs/**" };
+
+        return http.csrf(AbstractHttpConfigurer::disable) //
+                .cors(AbstractHttpConfigurer::disable)//
+                .httpBasic(AbstractHttpConfigurer::disable) //
+                .formLogin(AbstractHttpConfigurer::disable)//
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //
+                .authorizeHttpRequests(request -> request //
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow Options request
+                        .requestMatchers(unsecuredResources).permitAll() // Allow unsecured resources
+                        .anyRequest().authenticated()) // Authenticate all other requests
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
+
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+}
---
+++ src/main/java/com/cca/moodmeter/utils/UtilsService.java
@@ -0,0 +1,14 @@
+package com.cca.moodmeter.utils;
+
+/**
+ * @author pajimene
+ *
+ */
+public interface UtilsService {
+
+    /**
+     * @return
+     */
+    String getVersion();
+
+}
