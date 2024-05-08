a9105097fb6be3dbf8cef0602e609afdecd4dc5c
ivanmr14 <imitro@alumni.uv.es>
2024-05-08 11:59:40 +0200

    Cambios tras revisión

--- src/main/java/com/cca/moodmeter/person/model/PersonDto.java
+++ src/main/java/com/cca/moodmeter/person/model/PersonDto.java
@@ -4,8 +4,6 @@ public class PersonDto {
 
     private Integer id;
 
-    private String saga;
-
     private String username;
 
     private String email;
@@ -14,27 +12,7 @@ public class PersonDto {
 
     private String lastname;
 
-    private Long centerId;
-
-    private String grade;
-
-    private String businesscode;
-
-    private String pucode;
-
-    private String startDate;
-
-    private String jobRole;
-
-    private boolean entity_Ldap;
-
-    private String globalEmployeeId;
-
-    private String location;
-
     private Integer active;
-
-    private String endDate;
 
     public Integer getId() {
         return id;
@@ -42,14 +20,6 @@ public class PersonDto {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getSaga() {
-        return saga;
-    }
-
-    public void setSaga(String saga) {
-        this.saga = saga;
     }
 
     public String getUsername() {
@@ -84,92 +54,12 @@ public class PersonDto {
         this.lastname = lastname;
     }
 
-    public Long getCenterId() {
-        return centerId;
-    }
-
-    public void setCenterId(Long centerId) {
-        this.centerId = centerId;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    public String getBusinesscode() {
-        return businesscode;
-    }
-
-    public void setBusinesscode(String businesscode) {
-        this.businesscode = businesscode;
-    }
-
-    public String getPucode() {
-        return pucode;
-    }
-
-    public void setPucode(String pucode) {
-        this.pucode = pucode;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getJobRole() {
-        return jobRole;
-    }
-
-    public void setJobRole(String jobRole) {
-        this.jobRole = jobRole;
-    }
-
-    public boolean isEntity_Ldap() {
-        return entity_Ldap;
-    }
-
-    public void setEntity_Ldap(boolean entity_Ldap) {
-        this.entity_Ldap = entity_Ldap;
-    }
-
-    public String getGlobalEmployeeId() {
-        return globalEmployeeId;
-    }
-
-    public void setGlobalEmployeeId(String globalEmployeeId) {
-        this.globalEmployeeId = globalEmployeeId;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
     public Integer getActive() {
         return active;
     }
 
     public void setActive(Integer active) {
         this.active = active;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
     }
 
 }
